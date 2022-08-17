c46fa80203ebc229c63a1e9f2601e8abc46dae59
Ingrid <git@ingrids.email>
2022-08-17 16:19:23 +0200

    add basic spatial points implementation

---
+++ src/points.rs
@@ -0,0 +1,65 @@
+use rstar::{primitives::GeomWithData, RTree};
+
+enum CoordinateType {
+    Cartesian,
+    Geodetic,
+}
+
+type Point = GeomWithData<[f32; 3], usize>;
+
+struct Points {
+    tree: RTree<Point>,
+    lats: Vec<f32>,
+    lons: Vec<f32>,
+    elevs: Vec<f32>,
+    lafs: Vec<f32>,
+    ctype: CoordinateType,
+}
+
+impl Points {
+    pub fn from_latlons(
+        lats: Vec<f32>,
+        lons: Vec<f32>,
+        elevs: Vec<f32>,
+        lafs: Vec<f32>,
+        ctype: CoordinateType,
+    ) -> Self {
+        //TODO: ensure vecs are the same size
+        let radius_earth = 6371.0; //TODO define this somewhere more sensible?
+
+        let raw_points: Vec<Point> = match ctype {
+            CoordinateType::Cartesian => lats
+                .iter()
+                .zip(lons.iter())
+                .enumerate()
+                .map(|(i, (lat, lon))| Point::new([*lat, *lon, 0.0], i))
+                .collect(),
+            CoordinateType::Geodetic => lats
+                .iter()
+                .zip(lons.iter())
+                .enumerate()
+                .map(|(i, (lat, lon))| {
+                    Point::new(
+                        [
+                            (lat.to_radians().cos() * lon.to_radians().cos() * radius_earth),
+                            (lat.to_radians().cos() * lon.to_radians().sin() * radius_earth),
+                            (lat.to_radians().sin() * radius_earth),
+                        ],
+                        i,
+                    )
+                })
+                .collect(),
+        };
+
+        let mut tree = RTree::bulk_load(raw_points);
+
+        Self {
+            tree,
+            lats,
+            lons,
+            elevs,
+            lafs,
+            ctype,
+        }
+    }
+}
