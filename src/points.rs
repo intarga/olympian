9efac55b5bbd1e444079d2ad93509d0efba7c358
Ingrid <git@ingrids.email>
2023-03-29 11:19:33 +0200

    implement helper functions to calculate distances

--- src/points.rs
+++ src/points.rs
@@ -98,3 +98,30 @@ pub fn convert_coordinates(lat: f32, lon: f32, ctype: CoordinateType) -> (f32, f
         ),
     }
 }
+
+pub fn calc_distance(lat1: f32, lon1: f32, lat2: f32, lon2: f32, ctype: CoordinateType) -> f32 {
+    match ctype {
+        CoordinateType::Cartesian => {
+            let dx = lon1 - lon2;
+            let dy = lat1 - lat2;
+            (dx * dx + dy * dy).sqrt()
+        }
+        CoordinateType::Geodetic => {
+            // TODO: check latlon validity?
+            let lat1r = lat1.to_radians();
+            let lat2r = lat2.to_radians();
+            let lon1r = lon1.to_radians();
+            let lon2r = lon2.to_radians();
+
+            let ratio = lat1r.cos() * lon1r.cos() * lat2r.cos() * lon2r.cos()
+                + lat1r.cos() * lon1r.sin() * lat2r.cos() * lon2r.sin()
+                + lat1r.sin() * lat2r.sin();
+
+            ratio.acos() * util::RADIUS_EARTH
+        }
+    }
+}
+
+pub fn calc_distance_xyz(x0: f32, y0: f32, z0: f32, x1: f32, y1: f32, z1: f32) -> f32 {
+    ((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1) + (z0 - z1) * (z0 - z1)).sqrt()
+}
