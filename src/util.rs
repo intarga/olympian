f1a608e5bc5667602306ac1532bac2421da27063
Ingrid <git@ingrids.email>
2023-05-08 23:06:14 +0200

    implement util::compute_quantile

--- src/util.rs
+++ src/util.rs
@@ -14,3 +14,35 @@ pub fn subset<T: Copy>(array: &Vec<T>, indices: &Vec<usize>) -> Vec<T> {
 
     new_array
 }
+
+// TODO: replace assertions with errors or remove them
+pub fn compute_quantile(quantile: f32, array: &Vec<f32>) -> f32 {
+    let mut new_array: Vec<f32> = array.iter().copied().filter(|x| is_valid(*x)).collect();
+    new_array.sort_by(|a, b| a.total_cmp(b));
+
+    let n = new_array.len();
+
+    assert!(n > 0);
+
+    // get the quantile from the sorted array
+    let lower_index = (quantile * (n - 1) as f32).floor() as usize;
+    let upper_index = (quantile * (n - 1) as f32).ceil() as usize;
+    let lower_value = new_array[lower_index];
+    let upper_value = new_array[upper_index];
+    let lower_quantile = lower_index as f32 / (n - 1) as f32;
+    let upper_quantile = upper_index as f32 / (n - 1) as f32;
+    let exact_q = if lower_index == upper_index {
+        lower_value
+    } else {
+        assert!(upper_quantile > lower_quantile);
+        assert!(quantile >= lower_quantile);
+        let f = (quantile - lower_quantile) / (upper_quantile - lower_quantile);
+        assert!(f >= 0.);
+        assert!(f <= 1.);
+        lower_value + (upper_value - lower_value) * f
+    };
+
+    assert!(is_valid(exact_q));
+
+    exact_q
+}
