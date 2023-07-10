8e193683a86290e9d16db2ba1f84838abee83395
Ingrid <git@ingrids.email>
2023-07-10 19:33:45 +0200

    accept and handle data with holes in timeseries tests

--- src/qc_tests.rs
+++ src/qc_tests.rs
@@ -5,6 +5,7 @@ use crate::{
 use faer_core::Mat;
 use std::{error::Error, fmt::Display};
 
+// TODO: use thiserror
 #[derive(Debug)]
 pub enum QcError {
     InvalidInputShape(String),
@@ -41,30 +42,48 @@ pub enum Flag {
     Isolated,
 }
 
-pub fn dip_check(data: [f32; 3], high: f32, max: f32) -> Flag {
+pub fn dip_check(data: &[Option<f32>], high: f32, max: f32) -> Result<Flag, QcError> {
+    if data.len() != 3 {
+        return Err(QcError::InvalidInputShape("data".to_string()));
+    }
+
+    if data.contains(&None) {
+        return Ok(Flag::DataMissing);
+    }
+    let data: Vec<f32> = data.iter().map(|opt| opt.unwrap()).collect();
+
     if (data[2] < data[1] && data[0] < data[1]) || (data[2] > data[1] && data[0] > data[1]) {
         let diffsum = ((data[2] - data[1]).abs() + (data[1] - data[0]).abs()).abs();
         let diffdiff = ((data[2] - data[1]).abs() - (data[1] - data[0]).abs()).abs();
 
         if diffsum > high && diffdiff < (diffsum * 35. / 100.) {
-            return Flag::Warn;
+            return Ok(Flag::Warn);
         }
 
         if diffsum > max && diffdiff < (diffsum * 35. / 100.) {
-            return Flag::Fail;
+            return Ok(Flag::Fail);
         }
     }
-    Flag::Pass
+    Ok(Flag::Pass)
 }
 
-pub fn step_check(data: [f32; 2], high: f32, max: f32) -> Flag {
+pub fn step_check(data: &[Option<f32>], high: f32, max: f32) -> Result<Flag, QcError> {
+    if data.len() != 2 {
+        return Err(QcError::InvalidInputShape("data".to_string()));
+    }
+
+    if data.contains(&None) {
+        return Ok(Flag::DataMissing);
+    }
+    let data: Vec<f32> = data.iter().map(|opt| opt.unwrap()).collect();
+
     if (data[0] - data[1]).abs() > high {
-        return Flag::Warn;
+        return Ok(Flag::Warn);
     }
     if (data[0] - data[1]).abs() > max {
-        return Flag::Fail;
+        return Ok(Flag::Fail);
     }
-    Flag::Pass
+    Ok(Flag::Pass)
 }
 
 #[allow(clippy::too_many_arguments)]
