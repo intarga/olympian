ed643fc14418e1f3fc0bdb637d59efc83df6b5aa
Ingrid <git@ingrids.email>
2023-08-18 19:24:31 +0200

    simplify sct output

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,7 @@ use thiserror::Error;
 
 mod qc_tests;
 pub use qc_tests::{
-    buddy_check::buddy_check,
-    dip_check::dip_check,
-    sct::{sct, SctOutput},
-    step_check::step_check,
+    buddy_check::buddy_check, dip_check::dip_check, sct::sct, step_check::step_check,
 };
 
 mod util;
