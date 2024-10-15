ae578e780497960c01ac8d8aaa61871e33319c21
Ingrid <git@ingrids.email>
2024-10-15 17:21:44 +0200

    implement greater_than.rs

--- src/checks/consistency.rs
+++ src/checks/consistency.rs
@@ -1,2 +1,8 @@
+mod greater_than;
+pub use greater_than::greater_than;
+
 mod aggregate_less_than_set;
 pub use aggregate_less_than_set::aggregate_less_than_set;
+
+// TODO: Figure out the ideal container type (Analogous to [`crate::DataCache`]) to pass large
+// amounts of data into consistency checks
