5c36c336ba5454a3e681a1d1800995537a1ad558
Ingrid <git@ingrids.email>
2024-10-17 17:12:43 +0200

    better naming and logic in aggregate_less_than_set (now single_greater_than_element)

--- src/checks/consistency.rs
+++ src/checks/consistency.rs
@@ -1,8 +1,8 @@
 mod greater_than;
 pub use greater_than::greater_than;
 
-mod aggregate_less_than_set;
-pub use aggregate_less_than_set::aggregate_less_than_set;
+mod single_greater_than_element;
+pub use single_greater_than_element::single_greater_than_element;
 
 // TODO: Figure out the ideal container type (Analogous to [`crate::DataCache`]) to pass large
 // amounts of data into consistency checks
