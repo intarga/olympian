16834aaa1f995af1bc2a1b4d5f2bc26d2d3ef51d
Ingrid <git@ingrids.email>
2024-10-18 20:31:07 +0200

    change the naming and logic of the consistency check again
    I scrutinised the kvalobs code, and it's not very consistent, but this
    best matches the behaviour in a consistent way.
    
    The only thing left to consider is whether we should return separate
    flags for the sequence values. kvalobs flags only one of them (and is
    rather inconsistent about which one some flag the min/max while others
    flag only the first value weirdly). I will consult with the others in
    Confident WP5 and revisit this based on feedback.

--- src/checks/consistency.rs
+++ src/checks/consistency.rs
@@ -1,8 +1,8 @@
 mod greater_than;
 pub use greater_than::greater_than;
 
-mod single_greater_than_element;
-pub use single_greater_than_element::single_greater_than_element;
+mod min_greater_than_single;
+pub use min_greater_than_single::min_greater_than_single;
 
 // TODO: Figure out the ideal container type (Analogous to [`crate::DataCache`]) to pass large
 // amounts of data into consistency checks
