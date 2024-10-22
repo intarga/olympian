8496277ac084b56a40abed134d087738f586f0e2
Ingrid <git@ingrids.email>
2024-10-22 20:27:37 +0200

    implement not_equal

--- src/checks/consistency.rs
+++ src/checks/consistency.rs
@@ -13,5 +13,8 @@ pub use min_less_than_single::min_less_than_single;
 mod max_less_than_single;
 pub use max_less_than_single::max_less_than_single;
 
+mod not_equal;
+pub use not_equal::not_equal;
+
 // TODO: Figure out the ideal container type (Analogous to [`crate::DataCache`]) to pass large
 // amounts of data into consistency checks
