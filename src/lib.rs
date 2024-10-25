80ac9243bbb79248269655d0bc76cf1734d8edf5
Ingrid <git@ingrids.email>
2024-10-25 16:02:56 +0200

    export util::Timeseries in root

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@ use thiserror::Error;
 pub mod checks;
 
 mod util;
-pub use util::{spatial_tree::SpatialTree, DataCache, Flag, SingleOrVec};
+pub use util::{spatial_tree::SpatialTree, DataCache, Flag, SingleOrVec, Timeseries};
 
 /// Error type for Olympian
 #[derive(Error, Debug, Clone)]
