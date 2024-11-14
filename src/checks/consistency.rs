d9c3b59e17786e2a20f10f6456eec93d379602ab
Ingrid <git@ingrids.email>
2024-11-14 16:08:20 +0100

    simplify x_x_than_single checks from 4 to 3

--- src/checks/consistency.rs
+++ src/checks/consistency.rs
@@ -1,17 +1,14 @@
 mod greater_than;
 pub use greater_than::greater_than;
 
-mod min_greater_than_single;
-pub use min_greater_than_single::min_greater_than_single;
+mod single_greater_than_min;
+pub use single_greater_than_min::single_greater_than_min;
 
-mod max_greater_than_single;
-pub use max_greater_than_single::max_greater_than_single;
+mod single_less_than_max;
+pub use single_less_than_max::single_less_than_max;
 
-mod min_less_than_single;
-pub use min_less_than_single::min_less_than_single;
-
-mod max_less_than_single;
-pub use max_less_than_single::max_less_than_single;
+mod single_outside_sequence;
+pub use single_outside_sequence::single_outside_sequence;
 
 mod not_equal;
 pub use not_equal::not_equal;
---
+++ src/checks/consistency/single_outside_sequence.rs
@@ -0,0 +1,94 @@
+use crate::Flag;
+
+/// Compares a single value to a higher resolution sequence, where the single value should never
+/// be outside the range of the sequence (including an adjustment)
+///
+/// Returns:
+/// - [`Flag::DataMissing`] if the single value is missing,
+/// - [`Flag::Pass`] if this invariant is held (i.e the single value is greater than the minimum
+///   of the sequence plus the adjustment AND less than the maximum),
+/// - [`Flag::DataMissing`] if any of the elements is missing, as we cannot be sure a missing data
+///   point did not satisfy the invariant.
+/// - [`Flag::Fail`] otherwise.
+pub fn single_outside_sequence(
+    single: Option<f32>,
+    sequence: &[Option<f32>],
+    adjustment: f32,
+) -> Flag {
+    let single = match single {
+        Some(value) => value,
+        None => {
+            // If the single is missing, we can't do a check at all
+            return Flag::DataMissing;
+        }
+    };
+
+    // find the minimum value and maximum values in the sequence (None if they are all missing), as well as a bool
+    // indicating if any value was missing
+    let (minmax, missing) = sequence.iter().fold(
+        (None, false),
+        |acc: (Option<(f32, f32)>, bool), elem| match elem {
+            // if the element wasn't missing...
+            Some(value) => match acc.0 {
+                // set the min if there isn't already a min, or the element is lower
+                Some(minmax) => (Some((minmax.0.min(*value), minmax.1.max(*value))), acc.1),
+                None => (Some((*value, *value)), acc.1),
+            },
+            // if the element was missing, leave the min unchanged, but set `missing` to true
+            None => (acc.0, true),
+        },
+    );
+    let (min, max) = match minmax {
+        Some(value) => value,
+        // if min is None at this point, then all the elements of the sequence were missing...
+        None => {
+            // so we can't perform the check
+            return Flag::DataMissing;
+        }
+    };
+
+    if single > min + adjustment && single < max + adjustment {
+        // if this condition evaluates to true, the invariant held, i.e the single value is
+        // definitely within the sequence
+        Flag::Pass
+    } else if missing {
+        // if the condition was not met, but there was missing data in the sequence, we cannot say
+        // for sure that the invariant was invalidated
+        Flag::DataMissing
+    } else {
+        Flag::Fail
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_single_outside_sequence() {
+        assert_eq!(
+            single_outside_sequence(Some(1.), &[Some(1.), Some(2.), Some(2.)], -0.2),
+            Flag::Pass
+        );
+        assert_eq!(
+            single_outside_sequence(Some(1.), &[Some(1.), Some(2.), Some(2.)], 0.2),
+            Flag::Fail
+        );
+        assert_eq!(
+            single_outside_sequence(Some(2.), &[Some(1.), Some(2.), Some(2.)], -0.2),
+            Flag::Fail
+        );
+        assert_eq!(
+            single_outside_sequence(Some(2.), &[Some(1.), Some(2.), Some(2.)], 0.2),
+            Flag::Pass
+        );
+        assert_eq!(
+            single_outside_sequence(Some(1.), &[Some(1.), None, Some(2.)], -0.2),
+            Flag::Pass
+        );
+        assert_eq!(
+            single_outside_sequence(Some(1.), &[Some(1.), None, Some(2.)], 0.2),
+            Flag::DataMissing
+        );
+    }
+}
