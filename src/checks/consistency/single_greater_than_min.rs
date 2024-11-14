8a3e4d149c08a12cc0f8a064f7e2979bc16e9f4a
Ingrid <git@ingrids.email>
2024-11-14 16:08:20 +0100

    simplify x_x_than_single checks from 4 to 3

--- src/checks/consistency/single_greater_than_min.rs
+++ src/checks/consistency/single_greater_than_min.rs
@@ -1,16 +1,16 @@
 use crate::Flag;
 
-/// Compares a single value to a higher resolution sequence, where the maximum value in the
-/// sequence (including an adjustment) should never be less than the single value
+/// Compares a single value to a higher resolution sequence, where the single value should never
+/// be greater than the minimum value in the sequence (including an adjustment)
 ///
 /// Returns:
 /// - [`Flag::DataMissing`] if the single value is missing,
-/// - [`Flag::Fail`] if this invariant is broken (i.e the maximum of the sequence, plus the
-///   adjustment, is less than the single value),
+/// - [`Flag::Fail`] if this invariant is broken (i.e the single value is greater than the minimum
+///   of the sequence plus the adjustment),
 /// - [`Flag::DataMissing`] if any of the elements is missing, as we cannot be sure a missing data
 ///   point did not violate the invariant.
 /// - [`Flag::Pass`] otherwise.
-pub fn max_less_than_single(
+pub fn single_greater_than_min(
     single: Option<f32>,
     sequence: &[Option<f32>],
     adjustment: f32,
@@ -23,35 +23,35 @@ pub fn max_less_than_single(
         }
     };
 
-    // find the maximum value in the sequence (None if they are all missing), as well as a bool
+    // find the minimum value in the sequence (None if they are all missing), as well as a bool
     // indicating if any value was missing
-    let (max, missing) = sequence
+    let (min, missing) = sequence
         .iter()
         .fold((None, false), |acc: (Option<f32>, bool), elem| match elem {
             // if the element wasn't missing...
             Some(value) => match acc.0 {
-                // set the max if there isn't already a max, or the element is lower
-                Some(max) => (Some(max.max(*value)), acc.1),
+                // set the min if there isn't already a min, or the element is lower
+                Some(min) => (Some(min.min(*value)), acc.1),
                 None => (Some(*value), acc.1),
             },
-            // if the element was missing, leave the max unchanged, but set `missing` to true
+            // if the element was missing, leave the min unchanged, but set `missing` to true
             None => (acc.0, true),
         });
-    let max = match max {
+    let min = match min {
         Some(value) => value,
-        // if max is None at this point, then all the elements of the sequence were missing...
+        // if min is None at this point, then all the elements of the sequence were missing...
         None => {
             // so we can't perform the check
             return Flag::DataMissing;
         }
     };
 
-    if max + adjustment < single {
+    if single > min + adjustment {
         // if this condition evaluates to true, the invariant was invalidated
         Flag::Fail
     } else if missing {
         // if the condition was not met, but there was missing data in the sequence, we cannot say
-        // for sure that the invariant wasn't validated
+        // for sure that the invariant wasn't invalidated
         Flag::DataMissing
     } else {
         Flag::Pass
@@ -63,21 +63,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_max_less_than_single() {
+    fn test_single_greater_than_min() {
         assert_eq!(
-            max_less_than_single(Some(1.), &[Some(1.), Some(0.), Some(0.)], 0.2),
+            single_greater_than_min(Some(1.), &[Some(1.), Some(2.), Some(2.)], 0.2),
             Flag::Pass
         );
         assert_eq!(
-            max_less_than_single(Some(1.), &[Some(1.), Some(0.), Some(0.)], -0.2),
+            single_greater_than_min(Some(1.), &[Some(1.), Some(2.), Some(2.)], -0.2),
             Flag::Fail
         );
         assert_eq!(
-            max_less_than_single(Some(1.), &[Some(1.), None, Some(0.)], -0.2),
+            single_greater_than_min(Some(1.), &[Some(1.), None, Some(2.)], -0.2),
             Flag::Fail
         );
         assert_eq!(
-            max_less_than_single(Some(1.), &[Some(1.), None, Some(0.)], 0.2),
+            single_greater_than_min(Some(1.), &[Some(1.), None, Some(2.)], 0.2),
             Flag::DataMissing
         );
     }
--- src/checks/consistency/single_less_than_max.rs
+++ src/checks/consistency/single_less_than_max.rs
@@ -1,16 +1,16 @@
 use crate::Flag;
 
-/// Compares a single value to a higher resolution sequence, where the minimum value in the
-/// sequence (including an adjustment) should never be less than the single value
+/// Compares a single value to a higher resolution sequence, where the single value should never
+/// be less than the maximum value in the sequence (including an adjustment)
 ///
 /// Returns:
 /// - [`Flag::DataMissing`] if the single value is missing,
-/// - [`Flag::Fail`] if this invariant is broken (i.e the minimum of the sequence, plus the
-///   adjustment, is less than the single value),
+/// - [`Flag::Fail`] if this invariant is broken (i.e the single value is less than the maximum
+///   of the sequence plus the adjustment),
 /// - [`Flag::DataMissing`] if any of the elements is missing, as we cannot be sure a missing data
 ///   point did not violate the invariant.
 /// - [`Flag::Pass`] otherwise.
-pub fn min_less_than_single(
+pub fn single_less_than_max(
     single: Option<f32>,
     sequence: &[Option<f32>],
     adjustment: f32,
@@ -23,35 +23,35 @@ pub fn min_less_than_single(
         }
     };
 
-    // find the minimum value in the sequence (None if they are all missing), as well as a bool
+    // find the maximum value in the sequence (None if they are all missing), as well as a bool
     // indicating if any value was missing
-    let (min, missing) = sequence
+    let (max, missing) = sequence
         .iter()
         .fold((None, false), |acc: (Option<f32>, bool), elem| match elem {
             // if the element wasn't missing...
             Some(value) => match acc.0 {
-                // set the min if there isn't already a min, or the element is lower
-                Some(min) => (Some(min.min(*value)), acc.1),
+                // set the max if there isn't already a max, or the element is lower
+                Some(max) => (Some(max.max(*value)), acc.1),
                 None => (Some(*value), acc.1),
             },
-            // if the element was missing, leave the min unchanged, but set `missing` to true
+            // if the element was missing, leave the max unchanged, but set `missing` to true
             None => (acc.0, true),
         });
-    let min = match min {
+    let max = match max {
         Some(value) => value,
-        // if min is None at this point, then all the elements of the sequence were missing...
+        // if max is None at this point, then all the elements of the sequence were missing...
         None => {
             // so we can't perform the check
             return Flag::DataMissing;
         }
     };
 
-    if min + adjustment < single {
+    if single < max + adjustment {
         // if this condition evaluates to true, the invariant was invalidated
         Flag::Fail
     } else if missing {
         // if the condition was not met, but there was missing data in the sequence, we cannot say
-        // for sure that the invariant wasn't validated
+        // for sure that the invariant wasn't invalidated
         Flag::DataMissing
     } else {
         Flag::Pass
@@ -63,22 +63,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_min_less_than_single() {
+    fn test_single_less_than_max() {
         assert_eq!(
-            min_less_than_single(Some(1.), &[Some(1.), Some(2.), Some(2.)], 0.2),
+            single_less_than_max(Some(1.), &[Some(1.), Some(0.), Some(0.)], 0.2),
+            Flag::Fail
+        );
+        assert_eq!(
+            single_less_than_max(Some(1.), &[Some(1.), Some(0.), Some(0.)], -0.2),
             Flag::Pass
         );
         assert_eq!(
-            min_less_than_single(Some(1.), &[Some(1.), Some(2.), Some(2.)], -0.2),
-            Flag::Fail
-        );
-        assert_eq!(
-            min_less_than_single(Some(1.), &[Some(1.), None, Some(2.)], -0.2),
-            Flag::Fail
-        );
-        assert_eq!(
-            min_less_than_single(Some(1.), &[Some(1.), None, Some(2.)], 0.2),
+            single_less_than_max(Some(1.), &[Some(1.), None, Some(0.)], -0.2),
             Flag::DataMissing
+        );
+        assert_eq!(
+            single_less_than_max(Some(1.), &[Some(1.), None, Some(0.)], 0.2),
+            Flag::Fail
         );
     }
 }
