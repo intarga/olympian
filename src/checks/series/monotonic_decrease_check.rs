0979cda41b674a6e53d29168992141a764999a34
Ingrid <git@ingrids.email>
2024-11-25 19:53:58 +0100

    revamp monotonic_decrease_check_cache
    to flag each point based on ALL the windows that include it

--- src/checks/series/monotonic_decrease_check.rs
+++ src/checks/series/monotonic_decrease_check.rs
@@ -37,20 +37,32 @@ pub fn monotonic_decrease_check(data: &[Option<f32>], lower_limit: f32, upper_li
     }
 }
 
+// TODO: It might be possible to write a more efficent version of this function that doesn't call
+// down to `monotonic_decrease_check`
 /// Apply [`monotonic_decrease_check`] to a whole [`DataCache`]
 ///
-/// `window_size` here refers to the number of intervals to consider at once, as this is more
+/// `window_size` here refers to the number of intervals to consider at once (i.e. the number of
+/// points in a row that must be decreasing to be flagged as [`Flag::Fail`]), as this is more
 /// conceptually straightforward than the number of points (see docs for
 /// [`monotonic_decrease_check`]). Each iteration of `monotonic_decrease_check` will actually have
-/// `window_size` + 1 points passed to it.
+/// `window_size` + 1 points passed to it. That is to say, if you're QCing hourly data, and want to
+/// check for decreases over 24 hour periods, you should specify a `window_size` of 24, but
+/// internally when this function calls down to [`monotonic_decrease_check`] it will actually pass
+/// it 25 values.
 ///
-/// As `window_size` predecessors to each observation are needed, the [`SeriesCache`]
-/// provided must have `num_leading_points` >= `window_size`
+/// Since [`monotonic_decrease_check`] returns a flag relevant to the whole window passed in, but we
+/// want flags for individual data points, we consider flags [`monotonic_decrease check`] returns
+/// for every window that contains a given data point, and coalesce them according to the following
+/// precedence: [`Flag::Fail`] > [`Flag::DataMissing`] > [`Flag::Pass`].
+///
+/// As `window_size` predecessors and successors to each observation are needed to have all the
+/// windows relevent to the observation, the [`SeriesCache`] provided must have
+/// `num_leading_points` and `num_trailing_points` >= `window_size`
 ///
 /// ## Errors
 ///
 /// - data is invalid
-/// - data has `num_leading_points` < `window_size`
+/// - data has `num_leading_points` or `num_trailing_points` < `window_size`
 pub fn monotonic_decrease_check_cache(
     cache: &DataCache,
     lower_limit: f32,
@@ -66,28 +78,53 @@ pub fn monotonic_decrease_check_cache(
         // if this is none, the cache is empty, so we can just return an empty result vec
         None => return Ok(result_vec),
     };
+    let qced_series_len =
+        series_len - (cache.num_leading_points as usize + cache.num_trailing_points as usize);
 
     let (leading_trim, lead_overflow) = cache.num_leading_points.overflowing_sub(window_size);
+    let (trailing_trim, trail_overflow) = cache.num_trailing_points.overflowing_sub(window_size);
 
-    if lead_overflow || (leading_trim + window_size + 1) as usize > series_len {
+    if lead_overflow
+        || trail_overflow
+        || (leading_trim + trailing_trim + window_size + 1) as usize > series_len
+    {
         // TODO: nicer error here?
         return Err(Error::InvalidInputShape("data".to_string()));
     }
 
     for i in 0..num_series {
-        let trimmed = &cache.data[i].values
-            [leading_trim as usize..(series_len - cache.num_trailing_points as usize)];
+        let trimmed =
+            &cache.data[i].values[leading_trim as usize..(series_len - trailing_trim as usize)];
 
         // plus 1 to account for the extra data point needed to get an interval corresponding to
         // the first interval of the window
         let windows = trimmed.windows(window_size as usize + 1);
 
+        let window_flags: Vec<Flag> = windows
+            .map(|x| monotonic_decrease_check(x, lower_limit, upper_limit))
+            .collect();
+
+        // each point that actually needs to be QCed has `window_size + 1` windows that contain it,
+        // and so `window_size + 1` flags in `window_flags` associated with it which need to be
+        // coalesced, with `Fail` taking highest priority, then `DataMissing`.
+        let mut point_flags = Vec::with_capacity(qced_series_len);
+        for i in 0..qced_series_len {
+            let mut point_flag = Flag::Pass;
+            for j in 0..(window_size as usize + 1) {
+                if window_flags[i + j] == Flag::Fail {
+                    point_flag = Flag::Fail;
+                    break;
+                } else if window_flags[i + j] == Flag::DataMissing {
+                    point_flag = Flag::DataMissing;
+                }
+            }
+            point_flags.push(point_flag);
+        }
+
         result_vec.push(Timeseries {
             tag: cache.data[i].tag.clone(),
-            values: windows
-                .map(|x| monotonic_decrease_check(x, lower_limit, upper_limit))
-                .collect(),
-        });
+            values: point_flags,
+        })
     }
 
     Ok(result_vec)
@@ -124,102 +161,176 @@ mod tests {
 
     #[test]
     fn test_monotonic_decrease_check_cache() {
-        let decreasing_sequence: Vec<Option<f32>> = repeat(200.)
-            .take(27)
-            .enumerate()
-            .map(|(i, val)| Some(val - (i as f32 * 0.1)))
-            .collect();
-        let increase_in_middle = {
-            let mut s = decreasing_sequence.clone();
-            s[10] = Some(s[10].unwrap() + 0.2);
-            s
-        };
-        let increase_at_start = {
-            let mut s = decreasing_sequence.clone();
-            s[1] = Some(s[1].unwrap() + 0.2);
-            s
-        };
-        let increase_at_end = {
-            let mut s = decreasing_sequence.clone();
-            s[26] = Some(s[26].unwrap() + 0.2);
-            s
-        };
-        let missing_at_end = {
-            let mut s = decreasing_sequence.clone();
-            s[26] = None;
-            s
-        };
-        let big_decrease_at_end = {
-            let mut s = decreasing_sequence.clone();
-            s[26] = Some(s[26].unwrap() - 100.);
-            s
-        };
         assert_eq!(
             monotonic_decrease_check_cache(
                 &DataCache::new(
                     vec![
                         Timeseries {
                             tag: "blindern1".to_string(),
-                            values: decreasing_sequence
+                            // Should be [Fail, Fail]. All windows are slightly decreasing
+                            values: vec![
+                                Some(100.),
+                                Some(99.),
+                                Some(98.),
+                                Some(97.),
+                                Some(96.),
+                                Some(95.),
+                            ]
                         },
                         Timeseries {
                             tag: "blindern2".to_string(),
-                            values: increase_in_middle
+                            // Should be [Fail, Pass]. There's a decreasing window at 0..2, but
+                            // that window is only relevant for the first value
+                            values: vec![
+                                Some(100.),
+                                Some(99.),
+                                Some(98.),
+                                Some(100.),
+                                Some(100.),
+                                Some(100.),
+                            ]
                         },
                         Timeseries {
                             tag: "blindern3".to_string(),
-                            values: increase_at_start
+                            // Should be [Pass, Fail]. There's a decreasing window at 3..5, but
+                            // that window is only relevant for the second value
+                            values: vec![
+                                Some(100.),
+                                Some(100.),
+                                Some(100.),
+                                Some(100.),
+                                Some(100.),
+                                Some(95.),
+                            ]
                         },
                         Timeseries {
                             tag: "blindern4".to_string(),
-                            values: increase_at_end
+                            // Should be [Fail, Fail]. There's an non-decreasing window at 3..5,
+                            // but the second value still has 2 decreasing windows relevant to it
+                            values: vec![
+                                Some(100.),
+                                Some(99.),
+                                Some(98.),
+                                Some(97.),
+                                Some(96.),
+                                Some(97.),
+                            ]
                         },
                         Timeseries {
                             tag: "blindern5".to_string(),
-                            values: missing_at_end
+                            // Should be [Pass, Pass]. Always increasing, so no decreasing sequence
+                            values: vec![
+                                Some(100.),
+                                Some(101.),
+                                Some(102.),
+                                Some(103.),
+                                Some(104.),
+                                Some(105.),
+                            ]
                         },
                         Timeseries {
                             tag: "blindern6".to_string(),
-                            values: big_decrease_at_end
+                            // Should be [Pass, Pass]. The sequence is decreasing, but by less than
+                            // lower_limit
+                            values: vec![
+                                Some(100.),
+                                Some(99.9),
+                                Some(99.8),
+                                Some(99.7),
+                                Some(99.6),
+                                Some(99.5),
+                            ]
+                        },
+                        Timeseries {
+                            // Should be [Pass, DataMissing]. This along with the following case,
+                            // checks that DataMissing in one window takes precedence over Passes
+                            // in the others, but not over Fails
+                            tag: "blindern7".to_string(),
+                            values: vec![
+                                Some(100.),
+                                Some(101.),
+                                Some(102.),
+                                Some(103.),
+                                Some(104.),
+                                None,
+                            ]
+                        },
+                        Timeseries {
+                            // Should be [Fail, Fail]. See the above case for an explanation
+                            tag: "blindern8".to_string(),
+                            values: vec![
+                                Some(100.),
+                                Some(99.),
+                                Some(98.),
+                                Some(97.),
+                                Some(96.),
+                                None,
+                            ]
+                        },
+                        Timeseries {
+                            tag: "blindern9".to_string(),
+                            // Should be [Pass, Pass]. Represents the case of the bucket being
+                            // emptied. There are decreasing sequences, but they're by more than
+                            // upper_limit
+                            values: vec![
+                                Some(100.),
+                                Some(100.),
+                                Some(100.),
+                                Some(0.),
+                                Some(0.),
+                                Some(0.),
+                            ]
                         },
                     ],
-                    vec![0., 1., 2., 3., 4., 5.],
-                    vec![0., 1., 2., 3., 4., 5.],
-                    vec![0., 0., 0., 0., 0., 0.],
+                    vec![0., 1., 2., 3., 4., 5., 6., 7., 8.,],
+                    vec![0., 1., 2., 3., 4., 5., 6., 7., 8.,],
+                    vec![0., 0., 0., 0., 0., 0., 0., 0., 0.,],
                     crate::util::Timestamp(0),
                     RelativeDuration::minutes(10),
-                    24,
-                    0,
+                    2,
+                    2,
                 ),
-                0.7,
-                100.,
-                24
+                1.,
+                50.,
+                2
             )
             .unwrap(),
             vec![
                 Timeseries {
                     tag: "blindern1".to_string(),
-                    values: vec![Flag::Fail, Flag::Fail, Flag::Fail,]
+                    values: vec![Flag::Fail, Flag::Fail]
                 },
                 Timeseries {
                     tag: "blindern2".to_string(),
-                    values: vec![Flag::Pass, Flag::Pass, Flag::Pass,]
+                    values: vec![Flag::Fail, Flag::Pass]
                 },
                 Timeseries {
                     tag: "blindern3".to_string(),
-                    values: vec![Flag::Pass, Flag::Fail, Flag::Fail,]
+                    values: vec![Flag::Pass, Flag::Fail]
                 },
                 Timeseries {
                     tag: "blindern4".to_string(),
-                    values: vec![Flag::Fail, Flag::Fail, Flag::Pass,]
+                    values: vec![Flag::Fail, Flag::Fail]
                 },
                 Timeseries {
                     tag: "blindern5".to_string(),
-                    values: vec![Flag::Fail, Flag::Fail, Flag::DataMissing,]
+                    values: vec![Flag::Pass, Flag::Pass]
                 },
                 Timeseries {
                     tag: "blindern6".to_string(),
-                    values: vec![Flag::Fail, Flag::Fail, Flag::Pass,]
+                    values: vec![Flag::Pass, Flag::Pass]
+                },
+                Timeseries {
+                    tag: "blindern7".to_string(),
+                    values: vec![Flag::Pass, Flag::DataMissing]
+                },
+                Timeseries {
+                    tag: "blindern8".to_string(),
+                    values: vec![Flag::Fail, Flag::Fail]
+                },
+                Timeseries {
+                    tag: "blindern9".to_string(),
+                    values: vec![Flag::Pass, Flag::Pass]
                 },
             ]
         )
